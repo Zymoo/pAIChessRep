9bf19bf1a07876c8d24ece877fff5cd6b78d26c5
Tomasz Zymkowski <33435149+Zymoo@users.noreply.github.com>
2018-05-16 00:10:44 +0200

    Update fight.py

--- ProjektSI/fight.py
+++ ProjektSI/fight.py
@@ -2,7 +2,6 @@ import math
 import chess
 import chess.variant
 import random
-import pickle
 from organism import Organism
 
 def match(whitePlayer, blackPlayer, searchDepth):
@@ -30,34 +29,10 @@ def match(whitePlayer, blackPlayer, searchDepth):
             else:
                 return 0
 
-def evolution(searchDepth, primaryPopulation, numberOfGenerations, random):
+def evolution(searchDepth, primaryPopulation, numberOfGenerations):
+
+    population = initPopulation(primaryPopulation)
     generations = numberOfGenerations
-    if random == True:
-        population = initPopulation(primaryPopulation)
-    else:
-        population = []
-
-        print('Odpicklowanie startowe')
-        with open('chosenPopulation.pkl', 'rb') as input:
-            for x in range(0, 60):
-                unpickledOrganism = pickle.load(input)
-                population.append(unpickledOrganism)
-
-        print("Odpicklowane organizmy:")
-        licznik = 1
-        for organism in population:
-            print(str(licznik)+': ')
-            print(organism)
-            licznik=licznik+1
-
-        del population[:20]
-
-        print("Obcięte wrzucone do ewolucji:")
-        licznik = 1
-        for organism in population:
-            print(str(licznik)+': ')
-            print(organism)
-            licznik=licznik+1
 
     for generation in range(generations):
         print("Generation " +  str(generation) + "\n")
@@ -66,9 +41,14 @@ def evolution(searchDepth, primaryPopulation, numberOfGenerations, random):
         population.append(children[0])
         population.append(children[1])
         population = mutation(population)
+        for org in population:
+            print(org.playerParam)
+
+
 
     return population
 
+#[mobilityPar, territoryPar, controlPar, centerControlPar, pawnStructurePar, kingSafetyPar] = [5,10,5,5,8,10]
 def initPopulation(orgNumber):
     return [Organism([random.uniform(1,3), random.uniform(1,3), random.randint(10,30), random.randint(5,25), random.randint(10,30), random.randint(10,30), random.randint(0,10), random.randint(0,20)]) for _ in range(orgNumber)]
 
@@ -138,42 +118,13 @@ def mutation(population):
                  param = param + random.uniform(-(1/10)*param,(1/10)*param)
     return population
 
-#main
-depth = 3
-populationNumber = 40
-generations = 30
-#jesli random = False to czyta 60 organizmów z pliku chosenPopulation.pkl
-random = True
-finalPopulation = evolution(depth, populationNumber, generations, random)
-
-print('Finalna populacja:')
-for organism in finalPopulation:
-    print(organism)
-
+finalPopulation = evolution(1, 10, 30)
 for org in finalPopulation:
-    #print(org.playerParam)
-    org.fitness = match(org,Organism([1,1,5,10,5,5,8,10]),3) - match(Organism([1,1,5,10,5,5,8,10]),org, 3)
-    #print(org.fitness)
-
-print('Picklowanie ostatecznych')
-with open('finalPopulation.pkl', 'wb') as output:
-    for organism in finalPopulation:
-        pickle.dump(organism, output, pickle.HIGHEST_PROTOCOL)
-
-print('Odpicklowanie ostatecznych')
-finalPopulation = []
-with open('finalPopulation.pkl', 'rb') as input:
-    for x in range(0, populationNumber):
-        unpickledOrganism = pickle.load(input)
-        finalPopulation.append(unpickledOrganism)
-
-print('Finalna populacja po meczyku z Zymowym:')
-for organism in finalPopulation:
-    print(organism)
+    print(org.playerParam)
+    #org.fitness = match(org,Organism([5,10,5,5,8,10]),2) - match(Organism([5,10,5,5,8,10]),org, 2)
+    print(org.fitness)
 
 
-    
-    
 #firstOrganism = Organism([5,10,5,5,8,10])
 #secondOrganism = Organism([5,10,5,5,8,10])
 #
