ca3426d26de21ccb1e44a011031d7d56846ad411
Tomasz Zymkowski <33435149+Zymoo@users.noreply.github.com>
2018-05-20 22:50:43 +0200

    Update simpleMatch.py

--- ProjektSI/simpleMatch.py
+++ ProjektSI/simpleMatch.py
@@ -29,27 +29,43 @@ class SimpleMatch:
             if board.is_game_over():
                 break
 
-    def playerComputerMatch(self, computer, depth):
+    def playerComputerMatch(self, computer, depth, white):
         print("\n")
         board = chess.variant.KingOfTheHillBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+        if white == True:
+            while True:
+                move = input()
+                print(move)
+                board.push(chess.Move.from_uci(move))
+                print(board)
+                print("\n")
+                if board.is_game_over():
+                    break
 
-        while True:
-            move = input()
-            print(move)
-            board.push(chess.Move.from_uci(move))
-            print(board)
-            print("\n")
-            if board.is_game_over():
-                break
+                move = computer.alphabeta(board, depth, -float("inf"), float("inf"), False)[1]
+                board.push(move)
+                print(board)
+                print("\n")
+                if board.is_game_over():
+                    break
+        else:
+            while True:
+                move = computer.alphabeta(board, depth, -float("inf"), float("inf"), True)[1]
+                board.push(move)
+                print(board)
+                print("\n")
+                if board.is_game_over():
+                    break
+                move = input()
+                print(move)
+                board.push(chess.Move.from_uci(move))
+                print(board)
+                print("\n")
+                if board.is_game_over():
+                    break
 
-            move = computer.alphabeta(board, depth, -float("inf"), float("inf"), False)[1]
-            board.push(move)
-            print(board)
-            print("\n")
-            if board.is_game_over():
-                break
-
-firstOrganism = Organism([3,2,5,10,5,5,8,10])
-secondOrganism = Organism([3,2,5,10,5,5,8,10])
+firstOrganism = Organism([1.7882712821646762, 2.0117582809407675, 20, 8, 14, 12, 7, 10])
+secondOrganism = Organism([1.8121153745983598, 2.557559664770355, 11, 8, 16, 22, 7, 10])
 sm = SimpleMatch()
-score = sm.computersMatch(firstOrganism,secondOrganism, 3)
+score = sm.computersMatch(secondOrganism,firstOrganism, 3)
+#sm.playerComputerMatch(Organism([1.7882712821646762, 2.0117582809407675, 20, 8, 14, 12, 7, 10]),3, False);
