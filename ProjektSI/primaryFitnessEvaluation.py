c73f0c8fd9e4a2a9be5e62af8164516ebb88db9a
Dubores <jakubszreder98@gmail.com>
2018-05-20 22:26:34 +0200

    Update and rename PrimaryFitnessEvaluation.py to primaryFitnessEvaluation.py

--- ProjektSI/primaryFitnessEvaluation.py
+++ ProjektSI/primaryFitnessEvaluation.py
@@ -3,7 +3,6 @@ import chess
 import chess.variant
 import random
 from organism import Organism
-import pickle
 
 
 class PrimaryFitnessEvaluation:
@@ -25,27 +24,19 @@ class PrimaryFitnessEvaluation:
             gameScore = 0
             line = lines[random.randint(0, 49)]
             moves = line.split(" ")
-            #print(moves);
-
             whiteMoves = moves[1::3]
             blackMoves = moves[2::3]
             board = chess.variant.KingOfTheHillBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 
             for i in range(len(whiteMoves)):
                 board.push_san(whiteMoves[i])
-                #print(whiteMoves[i])
-                #print(board)
                 foundMove = (organism.alphabeta(board, depth, -float("inf"), float("inf"), False ))[1]
-                #print(foundMove)
-                #print(blackMoves[i])
                 if board.san(foundMove) == blackMoves[i]:
                     gameScore = gameScore + 1
                 board.push_san(blackMoves[i])
-                #print(board)
             gameScore = gameScore/len(whiteMoves)
             score += gameScore
         score = score/gamesRange
-        #print(score)
         return score
 
     def figthWhithBlackGameMaster(self, organism, gamesRange, depth):
@@ -60,32 +51,22 @@ class PrimaryFitnessEvaluation:
 
             whiteMoves = moves[1::3]
             blackMoves = moves[2::3]
-            #print(whiteMoves)
-            #print(blackMoves)
             board = chess.variant.KingOfTheHillBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 
             for i in range(len(whiteMoves) - 1):
                 foundMove = (organism.alphabeta(board, depth, -float("inf"), float("inf"), True))[1]
-                #print(foundMove)
-                #print(whiteMoves[i])
                 if board.san(foundMove) == whiteMoves[i]:
                     gameScore = gameScore + 1
                 board.push_san(whiteMoves[i])
-                #print(board)
                 board.push_san(blackMoves[i])
-                #print(blackMoves[i])
-                #print(board)
 
             foundMove = (organism.alphabeta(board, depth, -float("inf"), float("inf"), True))[1]
-            #print(foundMove)
-            #print(whiteMoves[-1])
             if board.san(foundMove) == whiteMoves[i]:
                 gameScore = gameScore + 1
 
             gameScore = gameScore/len(whiteMoves)
             score += gameScore
         score = score/gamesRange
-        #print(score)
         return score
 
     def select(self, numberOfGames, depth):
@@ -96,28 +77,6 @@ class PrimaryFitnessEvaluation:
         return sorted(self.population,key=lambda organism: organism.fitness)
 
 
+selec = PrimaryFitnessEvaluation(startNumber=60)
+organisms = selec.select(numberOfGames=2, depth=3)
 
-startPopulation = 4
-gamesNumber = 2
-depth = 1
-
-selec = PrimaryFitnessEvaluation(startPopulation)
-organisms = selec.select(gamesNumber, depth)
-for organism in organisms:
-    print(organism)
-
-print('Picklowanie')
-with open('chosenPopulation.pkl', 'wb') as output:
-    for organism in organisms:
-        pickle.dump(organism, output, pickle.HIGHEST_PROTOCOL)
-
-print('Odpicklowanie')
-chosenPopulation = []
-with open('chosenPopulation.pkl', 'rb') as input:
-    for x in range (0, startPopulation):
-        unpickledOrganism = pickle.load(input)
-        chosenPopulation.append(unpickledOrganism)
-
-print('Organizmy po początkowej selekcji:')
-for organism in chosenPopulation:
-    print(organism)
